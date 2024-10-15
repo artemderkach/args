a24034f62bc66c0148c60d6f7540c033bacd6401
artemderkach <artem.derkach.gs@gmail.com>
2024-10-15 23:35:29 +0300

    try things with parser

--- args.zig
+++ args.zig
@@ -1,5 +1,31 @@
 const std = @import("std");
 
-pub fn parse() void {
-    std.debug.print("hello!\n", .{});
+const command = struct {
+    name: []const u8,
+    commands: ?[]command,
+    options: ?[]option,
+};
+
+const option = struct {
+    long: []u8,
+    short: []u8,
+    type: type,
+};
+
+pub fn parse(arguments: [][]u8, _: command) void {
+    std.debug.print("hello! \n", .{});
+}
+
+test "parse" {
+    // const name = "serial";
+    const c = command{
+        .name = "serial",
+        .commands = null,
+        .options = null,
+    };
+
+    std.process.ArgIteratorGeneral()
+
+
+    parse(c);
 }
