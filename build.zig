b87009aef0d09e58f7d03be1990de1fb48e30e4c
artemderkach <artem.derkach.gs@gmail.com>
2024-10-15 21:02:26 +0300

    handle error when caller importing package with providing target and optimize

--- build.zig
+++ build.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("args", .{ .root_source_file = b.path("args.zig") });
+    _ = b.addModule("args", .{
+        .root_source_file = b.path("args.zig"),
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+    });
 }
