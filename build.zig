8b0e2ae4f288a074712ce90b1dfff01f4a5ca22d
artemderkach <artem.derkach.gs@gmail.com>
2024-10-15 20:30:02 +0300

    fix return value

--- build.zig
+++ build.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    b.addModule("args", .{ .root_source_file = b.path("args.zig") });
+    _ = b.addModule("args", .{ .root_source_file = b.path("args.zig") });
 }
