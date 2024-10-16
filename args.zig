eea6ae12fba99456bd939a59b8a0954de0589cc5
Artem Derkach <artem.derkach.gs@gmail.com>
2024-10-16 19:31:19 +0300

    next iteration for parser (accept iterator)

--- args.zig
+++ args.zig
@@ -1,31 +1,87 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
-const command = struct {
-    name: []const u8,
-    commands: ?[]command,
-    options: ?[]option,
+const IteratorTest = struct {
+    args: [][:0]u8,
+
+    pub fn next(_: *IteratorTest) ?([:0]const u8) {
+        return null;
+    }
 };
 
-const option = struct {
+const Iterator = if (builtin.is_test) IteratorTest else std.process.ArgIterator;
+
+const Command = struct {
+    name: []const u8,
+    arguments: ?[][]const u8,
+    commands: ?[]Command,
+    options: ?[]Option,
+};
+
+const Option = struct {
     long: []u8,
     short: []u8,
     type: type,
 };
 
-pub fn parse(arguments: [][]u8, _: command) void {
-    std.debug.print("hello! \n", .{});
+pub fn parse(arguments: []const [:0]const u8, iter: Iterator) void {
+    for (arguments) |arg| {
+        std.debug.print("==> {s}, {any}\n", .{ arg, @TypeOf(arg) });
+    }
+    std.debug.print("hello! argument: {any}, command: {any}\n", .{ arguments, iter });
 }
 
 test "parse" {
     // const name = "serial";
-    const c = command{
-        .name = "serial",
-        .commands = null,
-        .options = null,
+    // const c = Command{
+    //     .name = "serial",
+    //     .arguments = null,
+    //     .commands = null,
+    //     .options = null,
+    // };
+
+    const allocator = std.testing.allocator;
+    var iter = try std.process.ArgIterator.initWithAllocator(allocator);
+    defer iter.deinit();
+
+    while (iter.next()) |arg| {
+        std.debug.print("++> {s}, {any}\n", .{ arg, @TypeOf(arg) });
+    }
+    std.debug.print("--> {any}, {}\n", .{ std.os.argv, @TypeOf(std.os.argv) });
+
+    // const inp: [std.os.argv.len][:0]const u8 = undefined;
+    // std.debug.print("00000> {any}\n", .{@TypeOf(inp)});
+
+    // const a = [_:0]u8{ 'h', 'e', 'l', 'l' };
+    // std.debug.print("==> {s}, {any}\n", .{ a, @TypeOf(a) });
+    //
+    // const b = "hell";
+    // std.debug.print("==> {s}, {any}\n", .{ b, @TypeOf(b) });
+    //
+    // const c: []const u8 = "hell";
+    // std.debug.print("==> {s}, {any}\n", .{ c, @TypeOf(c) });
+
+    const e: []const [:0]const u8 = &.{
+        "aaa",
+        "-h",
+        "--hello",
     };
+    std.debug.print("--> {any}, {}\n", .{ std.os.argv, @TypeOf(std.os.argv) });
+    std.debug.print("--> {any}, {}\n", .{ @TypeOf(e), @TypeOf(&e) });
+    std.debug.print("--> {any}\n", .{@TypeOf(e[0..])});
 
-    std.process.ArgIteratorGeneral()
+    for (e) |arg| {
+        std.debug.print("==> {s}, {any}\n", .{ arg, @TypeOf(arg) });
+    }
 
+    parse(e, iter);
+    //
+    // const t = [_][:0]const u8{
+    //     [:0]u8{"some"},
+    // };
+    // for (t) |arg| {
+    //     std.debug.print("==> {s}, {any}\n", .{ arg, @TypeOf(arg) });
+    // }
 
-    parse(c);
+    // parse(c);
 }
